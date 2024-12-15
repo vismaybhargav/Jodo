e9abbd3394c06532690a8cac5187c92f4d217ec6
Vismay Bhargav <122339161+vismaybhargav@users.noreply.github.com>
2024-12-15 00:12:29 -0800

    (feat) some stuff

---
+++ src/main/java/module-info.java
@@ -0,0 +1,3 @@
+open module Jodo {
+    requires javafx.controls;
+}
---
+++ src/main/java/org/vismayb/jodo/Main.java
@@ -0,0 +1,14 @@
+package org.vismayb.jodo;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+    }
+}
